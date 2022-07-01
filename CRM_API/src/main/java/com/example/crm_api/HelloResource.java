6e1fe323e4919632e775b506cdb460e30deabe5e
Abdelrahman <eng.abdelrahman.mostafa.soliman.com>
2022-07-01 15:17:20 +0200

    api configuration

--- CRM_API/src/main/java/com/example/crm_api/HelloResource.java
+++ CRM_API/src/main/java/com/example/crm_api/HelloResource.java
@@ -1,6 +1,7 @@
 package com.example.crm_api;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
@@ -9,6 +10,13 @@ public class HelloResource {
     @GET
     @Path("/hello")@Produces("text/plain")
     public String hello() {
+        return "Hello, World!";
+    }
+
+    @POST
+    @Path("/submitTicket") @Produces("application/json")
+    public String SubmitATicket ()
+    {
         return "Hello, World!";
     }
 }
--- CRM_API/src/main/java/com/example/database_management/DBConnection.java
+++ CRM_API/src/main/java/com/example/database_management/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBConnection {
     private static Connection con;
-    public static void createConnection(String dbURL, String dbusername, String dbPassword) {
+    public static Connection createConnection(String dbURL, String dbusername, String dbPassword) {
         try {
             Class.forName("org.postgresql.Driver");
             //   con=DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/ecommerce", dbusername, dbPassword);
@@ -16,5 +16,24 @@ public class DBConnection {
         } catch (Exception ex) {
             System.out.println("exception at database connection" + ex);
         }
+        return con;
+    }
+    public static Connection stablishConnection (){
+        String Driver = "jdbc:postgresql://";
+        String HostName="bqeanzizwzknlsga1loq-postgresql.services.clever-cloud.com";
+        String DBName ="bqeanzizwzknlsga1loq";
+        String port ="5432";
+        String DBUrl=Driver+HostName+":"+port+"/"+DBName;
+
+        String DBUsereName="uirb3exldinkfciyp3su";
+        String Password = "CPNJKCLLeaJCxNF5L3CZ";
+
+        return DBConnection.createConnection(DBUrl,DBUsereName,Password);
+    }
+    public static Connection getCon (){
+        if (con != null){
+            return con;
+        }else
+            return stablishConnection();
     }
 }
