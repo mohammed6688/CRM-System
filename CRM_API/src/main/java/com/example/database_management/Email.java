de90f82957fbfe2f73f5f271ccdfc5a53fdd6c9f
Mustafa-Hammad <96911067+Mustafa-Hammad@users.noreply.github.com>
2022-07-04 11:13:41 +0200

    email

---
+++ CRM_API/src/main/java/com/example/database_management/Email.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.database_management;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+/**
+ *
+ * @author Mustafa Raed
+ */
+public class Email {
+private static final String FROM = "project.billingiti@gmail.com";
+private static final String PASS = "123456789billing";
+    public static void sendemail(String text, String to) {
+
+        // Sender's email ID needs to be mentioned
+      
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass 
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication(FROM, PASS);
+
+            }
+
+        });
+        //session.setDebug(true);
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(FROM));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("Vodafone CRM Team");
+
+            Multipart multipart = new MimeMultipart();
+
+//            MimeBodyPart attachmentPart = new MimeBodyPart();
+            MimeBodyPart textPart = new MimeBodyPart();
+
+            //                attachmentPart.attachFile(fileNamePdf);
+            textPart.setText(text);
+            //                "Dear customer, we would like to inform you that your request has been"
+//                        + " submitted with the number "+TicketID
+//                        +" submitted regarding "+Description
+//                        +" and the problem is being resolved within 48 working hours");
+            multipart.addBodyPart(textPart);
+//                multipart.addBodyPart(attachmentPart);
+
+            message.setContent(multipart);
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+        }
+    }
+}
